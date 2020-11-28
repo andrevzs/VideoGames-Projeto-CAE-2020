4113e220a71d81df1d67e8ce9d19f7f98c888358
Barr00s <61515563+Barr00s@users.noreply.github.com>
2020-11-28 17:57:34 -0300

    Update Grafico3-Generos.py

--- ArquivosFinais/Grafico3-Generos.py
+++ ArquivosFinais/Grafico3-Generos.py
@@ -1,90 +1,63 @@
-
+# Gráfico 3 (Barras) - Top 10 Gêneros mais vendidos
 import pandas as pd
-import math
-import matplotlib.pyplot as plt
 
-vg = pd.read_csv('VideoGames.csv', sep=',')
+# ler CSV
+vg = pd.read_csv('../VideoGames.csv')
 
-def graf2():
-    generos = ['ACTION', 'ADVENTURE', 'FIGHTING', 'MISC', 'PLATFORM', 'PUZZLE', 'RACING', 'ROLE-PLAYING', 'SHOOTER', 'SIMULATION', 'SPORTS', 'STRATEGY']
+# Gerar lista com total de vendas
+total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# Gerar lista de gêneros em ordem alfabética
+lstgen = ['Action', 'Adventure', 'Fighting', 'Misc', 'Platform', 'Puzzle',
+          'Racing', 'Role-Playing', 'Simulation', 'Shooter', 'Sports', 'Strategy']
 
-    rodar = True
-    while rodar is True:
-        valido1 = False
-        valido2 = False
 
-        # Usuário fornece gênero
-        print('= Opções de Gênero =\nAction, Sports, Misc, Role-Playing, Shooter,', '\nAdventure, Racing, Platform, Simulation, Fighting, Strategy, Puzzle')
-        while valido1 is False:
-            gen = input('\nDigite um gênero: ')
-            try:
-                str(gen)
-                if gen.upper() in generos:
-                    valido1 = True
-                else:
-                    print('Oops... esse não é um gênero válido!')
-                    valido1 = False
-            except ValueError:
-                print('Oops... esse não é um gênero válido!')
+# Função para somar valores de vendas
+def graf3(ano_i, ano_f):
+    # Execução para todas linhas do CSV
+    for i in range(len(vg)):
+        li = vg.iloc[i]
 
-        while valido1 is True:
-            # Listas com valores
-            n_critica   = []
-            n_players = []
+        glob = float(li['Global_Sales'])  # Selecionar coluna de vendas 'Global Sales' (Soma de todas regiões)
+        gnr = li['Genre']                 # Selecionar coluna de Gênero
+        ano = int(li['Year_of_Release'])  # Selecionar coluna do ano de lançamento
 
-            for i in range(len(vg)):
-                li = vg.iloc[i]
+        # Apenas se não houver um intevalo de anos selecionado
+        if ano_i == 0 and ano_f == 0:
+            # Se o genero da linha for igual ao genero atual checado, somar na posição i
+            for j in range(len(lstgen)):
+                if gnr == lstgen[j]:
+                    total[j] += glob
+        # Caso o usuário tenha definido um intervalo de anos
+        else:
+            if int(ano_i) <= ano <= int(ano_f):
+                for j in range(len(lstgen)):
+                    if gnr == lstgen[j]:
+                        total[j] += glob
 
-                genero = str(li['Genre'])
-                cscore = li['Critic_Score']
-                pscore = li['User_Score']
+    return total
 
-                # Se gênero = genero_fornecido:
-                if genero.upper() == gen.upper():
-                    if not math.isnan(float(cscore)):
-                        n_critica.append(cscore/10)  # Adiciona 'Critic Score' à lista critic
-                    if not math.isnan(float(pscore)):
-                        n_players.append(pscore)  # Adiciona 'User Score' à lista player
 
-            # Plot histograma barras agrupadas
-            # Eixo X: nota
-            # Eixo Y: frequência
-            plt.style.use('dark_background')
-            plt.rcParams['figure.facecolor'] = '#222831'
-            plt.rcParams['axes.facecolor'] = '#222831'
-            plt.rcParams['text.color'] = '#ececec'
-            plt.rcParams['xtick.color'] = '#ececec'
-            plt.rcParams['ytick.color'] = '#ececec'
-            plt.rcParams['axes.labelcolor'] = '#ececec'
-            fig, a = plt.subplots()
+op = input('Deseja escolher um intervalo de anos? [S/N] ')
+if op.upper() == 'N':
+    graf3(0, 0)
+if op.upper() == 'S':
+    ini = int(input('\nDigite o primeiro ano: '))
+    fin = int(input('Digite o segundo ano: '))
+    graf3(ini, fin)
 
-            a.hist([n_players, n_critica], rwidth=0.85, color=['#30475e', '#f2a365'], label=['Players', 'Crítica'])
+# Calcular 10 maiores valores
+top10 = []
 
-            a.set_xlabel('Nota Recebida', color='#ececec')
-            a.set_ylabel('Ocorrências da Nota', color='#ececec')
-            a.set_title(f'Notas da Crítica e dos Jogadores para o gênero {gen.capitalize()}', fontweight='bold')
+for a in range(0, 5):
+    max1 = 0
 
-            plt.grid(True, which='both', axis='y', alpha=0.5, linestyle=':', color='#ececec')
-            plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow=True, ncol=2)
-            plt.tight_layout()
-            plt.show()
+    for b in range(len(total)):
+        if int(total[b]) > max1:
+            max1 = total[b]
 
-            while valido2 is False:
-                repetir = input('\nDeseja exibir outro gênero?[S/N] ')
-                try:
-                    if str(repetir):
-                        valido2 = True
-                        rodar = True
-                    else:
-                        'Algo deu errado. Digite de novo por favor: '
-                        valido2 = False
-                        rodar = False
-                except ValueError:
-                    print('Algo deu errado. Digite de novo por favor: ')
-                    rodar = False
-                if repetir.upper() == 'N':
-                    exit()
-                if repetir.upper() == 'S':
-                    graf2()
+        total.remove(max1)
+        top10.append(max1)
 
-graf2()
+
+print(total)
+print(top10)
