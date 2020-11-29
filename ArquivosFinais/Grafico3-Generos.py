82c65198abe66f6ff5e55ae6bc745d5fd1268a0b
Barr00s <61515563+Barr00s@users.noreply.github.com>
2020-11-29 15:57:46 -0300

    Update Grafico3-Generos.py

--- ArquivosFinais/Grafico3-Generos.py
+++ ArquivosFinais/Grafico3-Generos.py
@@ -1,8 +1,9 @@
+  
 # Gráfico 3 (Barras) - Top 10 Gêneros mais vendidos
 import pandas as pd
 
 # ler CSV
-vg = pd.read_csv('../VideoGames.csv')
+vg = pd.read_csv('VideoGames.csv')
 
 # Gerar lista com total de vendas
 total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -33,31 +34,31 @@ def graf3(ano_i, ano_f):
                 for j in range(len(lstgen)):
                     if gnr == lstgen[j]:
                         total[j] += glob
-
-    return total
+    top10vendas = []
+    top10gen = []
+    for k in range(0,10):
+        maior = max(total)
+        pos = total.index(maior)
+        top10gen.append(lstgen[pos])
+        top10vendas.append(maior)
+        total.remove(maior)
+        lstgen.remove(lstgen[pos])
+        
+    return top10gen, top10vendas
+    
 
 
 op = input('Deseja escolher um intervalo de anos? [S/N] ')
 if op.upper() == 'N':
-    graf3(0, 0)
+    k = graf3(0, 0)
 if op.upper() == 'S':
     ini = int(input('\nDigite o primeiro ano: '))
     fin = int(input('Digite o segundo ano: '))
-    graf3(ini, fin)
+    k = graf3(ini, fin)
 
 # Calcular 10 maiores valores
-top10 = []
-
-for a in range(0, 5):
-    max1 = 0
-
-    for b in range(len(total)):
-        if int(total[b]) > max1:
-            max1 = total[b]
-
-        total.remove(max1)
-        top10.append(max1)
 
 
-print(total)
-print(top10)
+
+print(k[0])
+print('\n\n\n\n', k[1])
