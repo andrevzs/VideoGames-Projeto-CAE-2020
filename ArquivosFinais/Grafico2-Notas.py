8b5295aa75d6f6397523ce5c3e39b521bbcafa88
Barr00s <61515563+Barr00s@users.noreply.github.com>
2020-11-29 15:19:19 -0300

    Update Grafico2-Notas.py

--- ArquivosFinais/Grafico2-Notas.py
+++ ArquivosFinais/Grafico2-Notas.py
@@ -1,7 +1,7 @@
-
 import pandas as pd
 import math
 import matplotlib.pyplot as plt
+
 
 vg = pd.read_csv('VideoGames.csv', sep=',')
 
@@ -14,18 +14,18 @@ def graf2():
         valido2 = False
 
         # Usuário fornece gênero
-        print('= Opções de Gênero =\nAction, Sports, Misc, Role-Playing, Shooter,', '\nAdventure, Racing, Platform, Simulation, Fighting, Strategy, Puzzle')
+        print('= Opcoes de Genero =\nAction, Sports, Misc, Role-Playing, Shooter,', '\nAdventure, Racing, Platform, Simulation, Fighting, Strategy, Puzzle')
         while valido1 is False:
-            gen = input('\nDigite um gênero: ')
+            gen = input('\nDigite um genero: ')
             try:
                 str(gen)
                 if gen.upper() in generos:
                     valido1 = True
                 else:
-                    print('Oops... esse não é um gênero válido!')
+                    print('Oops... esse nao e um genero valido!')
                     valido1 = False
             except ValueError:
-                print('Oops... esse não é um gênero válido!')
+                print('Oops... esse nao e um genero valido!')
 
         while valido1 is True:
             # Listas com valores
@@ -58,33 +58,16 @@ def graf2():
             plt.rcParams['axes.labelcolor'] = '#ececec'
             fig, a = plt.subplots()
 
-            a.hist([n_players, n_critica], rwidth=0.85, color=['#30475e', '#f2a365'], label=['Players', 'Crítica'])
+            a.hist([n_players, n_critica], rwidth=0.85, color=['#30475e', '#f2a365'], label=['Players', 'Critica'])
 
             a.set_xlabel('Nota Recebida', color='#ececec')
-            a.set_ylabel('Ocorrências da Nota', color='#ececec')
-            a.set_title(f'Notas da Crítica e dos Jogadores para o gênero {gen.capitalize()}', fontweight='bold')
+            a.set_ylabel('Ocorrencias da Nota', color='#ececec')
+            a.set_title(f'Notas da Critica e dos Jogadores para o genero {gen.capitalize()}', fontweight='bold')
 
             plt.grid(True, which='both', axis='y', alpha=0.5, linestyle=':', color='#ececec')
             plt.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05), shadow=True, ncol=2)
             plt.tight_layout()
             plt.show()
-
-            while valido2 is False:
-                repetir = input('\nDeseja exibir outro gênero?[S/N] ')
-                try:
-                    if str(repetir):
-                        valido2 = True
-                        rodar = True
-                    else:
-                        'Algo deu errado. Digite de novo por favor: '
-                        valido2 = False
-                        rodar = False
-                except ValueError:
-                    print('Algo deu errado. Digite de novo por favor: ')
-                    rodar = False
-                if repetir.upper() == 'N':
-                    exit()
-                if repetir.upper() == 'S':
-                    graf2()
-
+            valido1=False
+            rodar=False
 graf2()
