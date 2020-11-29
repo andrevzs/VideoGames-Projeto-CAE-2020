28ee56749b79ec7ee1f07a966229f44046e8337a
Barr00s <61515563+Barr00s@users.noreply.github.com>
2020-11-29 19:36:48 -0300

    grafico pronto :sunglasses: :sunglasses:

--- ArquivosFinais/Grafico4-Jogos.py
+++ ArquivosFinais/Grafico4-Jogos.py
@@ -1,6 +1,6 @@
 # Gráfico 4 - Total de Jogos /Plataforma
 import pandas as pd
-
+import matplotlib.pyplot as plt
 vg = pd.read_csv('VideoGames.csv', sep=',')
 
 
@@ -23,7 +23,7 @@ def graf4():
             console.append(platf)
             njogos.append(1)
 
-    for j in range(len(console)):
+    for j in range(0, 10):
         mxm = max(njogos)
         top_njogos.append(mxm)
 
@@ -37,5 +37,18 @@ def graf4():
 
 x = graf4()
 
-for a in range(len(x[0])):
-    print(f'O console {x[0][a]} recebeu {x[1][a]} jogos.')
+def plot(v):
+    plt.style.use('dark_background')
+    plt.rcParams['figure.facecolor'] = '#222831' 
+    plt.rcParams['axes.facecolor'] = '#222831'
+    plt.rcParams['text.color'] = '#ececec'
+    plt.rcParams['xtick.color'] = '#ececec'
+    plt.rcParams['ytick.color'] = '#ececec'
+    plt.rcParams['axes.labelcolor'] = '#ececec'
+    plt.title('Quantidade de jogos lançados para cada console')
+    plt.xlabel('Consoles')
+    plt.ylabel('Jogos')
+    plt.bar(v[0], v[1])
+    plt.show()
+
+plot(x)
